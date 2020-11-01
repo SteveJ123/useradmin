a3b18ab97a5f0a1cafdda29e7e2733077e113b47
unknown <jjanardhan@gmail.com>
2020-11-01 18:30:52 +0530

    added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'useradmin';
+    
+  title = "user name";
+  setElementUser=false;
+  setElementAdmin=false;
+  login=true;
+
+  onClickButton($event){  
+      $event.preventDefault();
+     var username = (<HTMLInputElement>document.getElementById("username-field")).value;
+     var password = (<HTMLInputElement>document.getElementById("password-field")).value;
+
+     if(username==="user" && password==="password"){
+      this.setElementUser=true;
+      this.login=false;
+     }
+
+     if(username==="admin" && password==="admin"){
+      console.log("click event");
+      this.setElementAdmin=true;
+      this.login=false;
+     }
+    // this.title = username;
+    
+  }
+
+  
+
+
 }
